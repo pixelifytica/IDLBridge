5f483d5ad52116dcbab2c4320e5f6336a4351382
ameak <alex.meakins@ccfe.ac.uk>
2014-07-22 14:49:59 +0100

    get() now supports array types (excluding arrays of structures).

--- idlbridge.pyx
+++ idlbridge.pyx
@@ -1,9 +1,15 @@
 # TODO: add license
 
-from numpy cimport ndarray
+from libc.string cimport memcpy
+from libc.stdlib cimport malloc, free
+import numpy as np
+cimport numpy as np
 
 # include IDL callable interface
 include "idl_export.pxi"
+
+# initialise numpy c-api
+np.import_array()
 
 
 class IDLLibraryError(Exception):
@@ -33,11 +39,10 @@ class IDLValueError(ValueError):
     An IDL Value Error Exception.
 
     This exception is thrown when a value is passed to the IDL library that it
-    isunable to handle.
+    is unable to handle.
     """
 
     pass
-
 
 
 # global variables used to track the usage of the IDL library
@@ -45,7 +50,7 @@ __references__ = 0
 __shutdown__ = False
 
 
-cdef _initialise_idl(bint quiet=False):
+cdef _initialise_idl(bint quiet=True):
 
     cdef IDL_INIT_DATA init_data
     global __shutdown__
@@ -116,11 +121,23 @@ cdef class IDLBridge:
         _deregister()
 
     cpdef object execute(self, str command):
+        """
+        Executes a string as an IDL command.
+
+        :param command: A string defining the idl command e.g. "d = 5.1"
+        :return: None
+        """
 
         byte_string = command.encode("UTF8")
         IDL_ExecuteStr(byte_string)
 
     cpdef object get(self, str variable):
+        """
+        Returns a python copy of the specified IDL variable.
+
+        :param variable: A string containing the variable name.
+        :return: The IDL variable data converted to python form.
+        """
 
         cdef IDL_VPTR vptr
 
@@ -145,54 +162,18 @@ cdef class IDLBridge:
 
                 return self._get_array(vptr)
 
-        elif vptr.type == IDL_TYP_BYTE:
-
-            return vptr.value.c
-
-        elif vptr.type == IDL_TYP_INT:
-
-            return vptr.value.i
-
-        elif vptr.type == IDL_TYP_UINT:
-
-            return vptr.value.ui
-
-        elif vptr.type == IDL_TYP_LONG:
-
-            return vptr.value.l
-
-        elif vptr.type == IDL_TYP_ULONG:
-
-            return vptr.value.ul
-
-        elif vptr.type == IDL_TYP_LONG64:
-
-            return vptr.value.l64
-
-        elif vptr.type ==  IDL_TYP_ULONG64:
-
-            return vptr.value.ul64
-
-        elif vptr.type == IDL_TYP_FLOAT:
-
-            return vptr.value.f
-
-        elif vptr.type == IDL_TYP_DOUBLE:
-
-            return vptr.value.d
-
-        elif vptr.type == IDL_TYP_COMPLEX:
-
-            return complex(vptr.value.cmp.r, vptr.value.cmp.i)
-
-        elif vptr.type == IDL_TYP_DCOMPLEX:
-
-            return complex(vptr.value.dcmp.r, vptr.value.dcmp.i)
-
-        elif vptr.type == IDL_TYP_STRING:
-
-            return self._get_string(vptr)
-
+        elif vptr.type == IDL_TYP_BYTE: return vptr.value.c
+        elif vptr.type == IDL_TYP_INT: return vptr.value.i
+        elif vptr.type == IDL_TYP_UINT: return vptr.value.ui
+        elif vptr.type == IDL_TYP_LONG: return vptr.value.l
+        elif vptr.type == IDL_TYP_ULONG: return vptr.value.ul
+        elif vptr.type == IDL_TYP_LONG64: return vptr.value.l64
+        elif vptr.type == IDL_TYP_ULONG64: return vptr.value.ul64
+        elif vptr.type == IDL_TYP_FLOAT: return vptr.value.f
+        elif vptr.type == IDL_TYP_DOUBLE: return vptr.value.d
+        elif vptr.type == IDL_TYP_COMPLEX: return complex(vptr.value.cmp.r, vptr.value.cmp.i)
+        elif vptr.type == IDL_TYP_DCOMPLEX: return complex(vptr.value.dcmp.r, vptr.value.dcmp.i)
+        elif vptr.type == IDL_TYP_STRING: return self._string_idl_to_py(vptr.value.str)
         elif vptr.type == IDL_TYP_PTR:
 
             raise NotImplementedError("Pointer types are not supported.")
@@ -205,25 +186,108 @@ cdef class IDLBridge:
 
             raise IDLTypeError("Unrecognised IDL type.")
 
-    cdef inline ndarray _get_array(self, IDL_VPTR vptr):
+    cdef inline np.ndarray _get_array(self, IDL_VPTR vptr):
+        """
+        Converts an IDL array to an equivalent numpy array.
 
-        # TODO: implement me
-        raise NotImplementedError("Not currently implemented.")
+        :param vptr: An IDL variable pointer pointing to an IDL array definition.
+        :return: A numpy array.
+        """
 
-    cdef inline dict _get_structure(self, IDL_VPTR vptr):
+        # All arrays MUST be copied otherwise changes in python or IDL will cause segfaults in one of the other
 
-        raise NotImplementedError("Not currently implemented.")
+        # Different array types unfortunately need special handling.
+        if vptr.type == IDL_TYP_STRING:
 
-    cdef inline str _get_string(self, IDL_VPTR vptr):
+            return self._get_array_string(vptr)
 
-        # The string pointer in the IDL string structure is invalid when the string length is zero.
-        if vptr.value.str.slen == 0:
+        elif vptr.type == IDL_TYP_STRUCT:
 
-            return ""
+            return self._get_array_structure(vptr)
 
         else:
 
-            return vptr.value.str.s.decode("UTF8")
+            return self._get_array_scalar(vptr)
+
+    cdef inline np.ndarray _get_array_string(self, IDL_VPTR vptr):
+        """
+        Converts an IDL string array to an equivalent numpy array.
+
+        :param vptr: An IDL variable pointer pointing to an IDL string array definition.
+        :return: A numpy array.
+        """
+
+        cdef:
+            IDL_STRING idl_string
+            list strings
+            int index
+            np.ndarray array
+            np.PyArray_Dims new_dimensions
+
+        # The idl string array is unpacked to form a flat list of strings, this is then
+        # converted to a numpy array before finally being reshaped to the correct dimensions.
+
+        # convert all the IDL strings in the array to python strings
+        strings = []
+        for index in range(vptr.value.arr.n_elts):
+
+            # dereference idl string pointer
+            idl_string = (<IDL_STRING *> (vptr.value.arr.data + index * vptr.value.arr.elt_len))[0]
+            strings.append(self._string_idl_to_py(idl_string))
+
+        # convert to a flat numpy array
+        array = np.array(strings)
+
+        # reshape array
+        new_dimensions.len = vptr.value.arr.n_dim
+        new_dimensions.ptr = self._dimensions_idl_to_numpy(vptr.value.arr.n_dim, vptr.value.arr.dim)
+        return np.PyArray_Newshape(array, &new_dimensions, np.NPY_CORDER)
+
+    cdef inline np.ndarray _get_array_structure(self, IDL_VPTR vptr):
+        """
+        To be written.
+
+        :param vptr:
+        :return:
+        """
+
+        return NotImplemented
+
+    cdef inline np.ndarray _get_array_scalar(self, IDL_VPTR vptr):
+        """
+        Converts an IDL scalar array to an equivalent numpy array.
+
+        :param vptr: An IDL variable pointer pointing to an IDL scalar array definition.
+        :return: A numpy array.
+        """
+
+        cdef:
+            int num_dimensions
+            np.npy_intp dimensions[IDL_MAX_ARRAY_DIM]
+            np.ndarray array
+            int index
+
+        # obtain numpy data type
+        numpy_type = self._type_idl_to_numpy(vptr.type)
+
+        # IDL defines its dimensions in the opposite order to numpy
+        numpy_dimensions = self._dimensions_idl_to_numpy(vptr.value.arr.n_dim, vptr.value.arr.dim)
+
+        # generate an empty numpy array and copy IDL array data
+        array = np.PyArray_SimpleNew(vptr.value.arr.n_dim, numpy_dimensions, numpy_type)
+        memcpy(array.data, <void *> vptr.value.arr.data, vptr.value.arr.arr_len)
+
+        return array
+
+    cdef inline dict _get_structure(self, IDL_VPTR vptr):
+        """
+        To be written.
+
+        :param vptr:
+        :return:
+        """
+
+        raise NotImplementedError("Not currently implemented.")
 
     cpdef object put(self, str variable, object data):
 
@@ -249,6 +313,121 @@ cdef class IDLBridge:
     cpdef object export_procedure(self, str name):
 
         return IDLProcedure(name, idl_bridge=self)
+
+    cdef inline str _string_idl_to_py(self, IDL_STRING string):
+        """
+        Converts an IDL string object to a python string.
+
+        :param string: An IDL string structure.
+        :return: A python string object.
+        """
+
+        # The string pointer in the IDL string structure is invalid when the string length is zero.
+        if string.slen == 0:
+
+            return ""
+
+        else:
+
+            return string.s.decode("UTF8")
+
+    cdef inline int _type_idl_to_numpy(self, int type):
+        """
+        Maps IDL type values to numpy type values.
+
+        :param type: An IDL type value.
+        :return: A numpy type value.
+        """
+
+        if type == IDL_TYP_INT: return np.NPY_INT16
+        elif type == IDL_TYP_LONG: return np.NPY_INT32
+        elif type == IDL_TYP_LONG64: return np.NPY_INT64
+        elif type == IDL_TYP_BYTE: return np.NPY_UINT8
+        elif type == IDL_TYP_UINT: return np.NPY_UINT16
+        elif type == IDL_TYP_ULONG: return np.NPY_UINT32
+        elif type == IDL_TYP_ULONG64: return np.NPY_UINT64
+        elif type == IDL_TYP_FLOAT: return np.NPY_FLOAT32
+        elif type == IDL_TYP_DOUBLE: return np.NPY_FLOAT64
+        elif type == IDL_TYP_COMPLEX: return np.NPY_COMPLEX64
+        elif type == IDL_TYP_DCOMPLEX: return np.NPY_COMPLEX128
+        elif type == IDL_TYP_STRING: return np.NPY_STRING
+
+        else:
+
+            raise IDLTypeError("No matching Numpy data type defined for given IDL type.")
+
+    cdef inline int _type_numpy_to_idl(self, int type):
+        """
+        Maps numpy type values to IDL type values.
+
+        :param type: A numpy type value.
+        :return: An IDL type value.
+        """
+
+        if type == np.NPY_INT8:
+
+            raise IDLTypeError("IDL does not support signed bytes.")
+
+        elif type == np.NPY_INT16: return IDL_TYP_INT
+        elif type == np.NPY_INT32: return IDL_TYP_LONG
+        elif type == np.NPY_INT64: return IDL_TYP_LONG64
+        elif type == np.NPY_UINT8: return IDL_TYP_BYTE
+        elif type == np.NPY_UINT16: return IDL_TYP_UINT
+        elif type == np.NPY_UINT32: return IDL_TYP_ULONG
+        elif type == np.NPY_UINT64: return IDL_TYP_ULONG64
+        elif type == np.NPY_FLOAT32: return IDL_TYP_FLOAT
+        elif type == np.NPY_FLOAT64: return IDL_TYP_DOUBLE
+        elif type == np.NPY_COMPLEX64: return IDL_TYP_COMPLEX
+        elif type == np.NPY_COMPLEX128: return IDL_TYP_DCOMPLEX
+        elif type == np.NPY_STRING: return IDL_TYP_STRING
+
+        else:
+
+            raise IDLTypeError("No matching IDL data type defined for given Numpy type.")
+
+    cdef inline np.npy_intp *_dimensions_idl_to_numpy(self, int dimension_count, IDL_ARRAY_DIM idl_dimensions):
+        """
+        Converts an IDL dimension array to a numpy dimension array.
+
+        IDL defines dimensions in the opposite order to numpy.
+        This method inverts the dimension order.
+
+        :param type: An IDL dimension array.
+        :return: A numpy dimension array.
+        """
+
+        cdef:
+            np.npy_intp numpy_dimensions[IDL_MAX_ARRAY_DIM]
+            int index
+
+        # IDL defines its dimensions with the opposite order to numpy, invert the order
+        for index in range(dimension_count):
+
+            numpy_dimensions[index] = idl_dimensions[dimension_count - (index + 1)]
+
+        return numpy_dimensions
+
+    cdef inline IDL_MEMINT *_dimensions_numpy_to_idl(self, int dimension_count, np.npy_intp *numpy_dimensions):
+        """
+        Converts a numpy dimension array to an IDL dimension array.
+
+        IDL defines dimensions in the opposite order to numpy.
+        This method inverts the dimension order.
+
+        :param type: A numpy dimension array.
+        :return: An IDL dimension array.
+        """
+
+        cdef:
+            IDL_ARRAY_DIM idl_dimensions
+            int index
+
+        # IDL defines its dimensions with the opposite order to numpy, invert the order
+        for index in range(dimension_count):
+
+            idl_dimensions[index] = numpy_dimensions[dimension_count - (index + 1)]
+
+        return idl_dimensions
 
 
 class _IDLCallable:
--- tests/test_idlbridge.py
+++ tests/test_idlbridge.py
@@ -2,64 +2,195 @@
 
 from unittest import TestCase
 import idlbridge as idl
+import numpy as np
 
-class test_idlbridge(TestCase):
+
+class TestIDLBridge(TestCase):
+
+    def assertArrayEqual(self, first, second, message):
+
+        # check data types are the same
+        self.assertEqual(first.dtype, second.dtype, message + " Data types are different.")
+
+        # check all elements are equal
+        self.assertTrue((first == second).any(), message + " Elements are not the same.")
 
     def test_execute(self):
 
-        # This fuction is difficult to test as it requires the other functions.
+        # This function is difficult to test as it requires the other functions.
         # Here we just test the command executes without a failure.
+        # Basically if this fails evey test is going to fail!
         idl.execute("d = 128")
 
-    def test_get_scalar(self):
+    # TODO: add test for null -> None
+
+    def test_get_scalar_byte(self):
 
         # unsigned byte
         idl.execute("test_byte = byte(15)")
         self.assertEqual(15, idl.get("test_byte"), "Failed to get unsigned byte.")
 
+    def test_get_scalar_int(self):
+
         # signed integer
         idl.execute("test_int = fix(-3485)")
         self.assertEqual(-3485, idl.get("test_int"), "Failed to get signed integer.")
+
+    def test_get_scalar_uint(self):
 
         # unsigned integer
         idl.execute("test_uint = uint(36640)")
         self.assertEqual(36640, idl.get("test_uint"), "Failed to get unsigned integer.")
 
+    def test_get_scalar_long(self):
+
         # signed long
         idl.execute("test_long = long(-1633485)")
         self.assertEqual(-1633485, idl.get("test_long"), "Failed to get signed long.")
+
+    def test_get_scalar_ulong(self):
 
         # unsigned long
         idl.execute("test_ulong = ulong(3267987588)")
         self.assertEqual(3267987588, idl.get("test_ulong"), "Failed to get unsigned long.")
 
+    def test_get_scalar_long64(self):
+
         # signed long64
         idl.execute("test_long64 = long64(-16000000096790)")
         self.assertEqual(-16000000096790, idl.get("test_long64"), "Failed to get signed long64.")
+
+    def test_get_scalar_ulong64(self):
 
         # unsigned long64
         idl.execute("test_ulong64 = ulong64(18446728073709454827)")
         self.assertEqual(18446728073709454827, idl.get("test_ulong64"), "Failed to get unsigned long64.")
 
+    def test_get_scalar_float(self):
+
         # float
         idl.execute("test_float = float(-2.0)")
         self.assertEqual(-2.0, idl.get("test_float"), "Failed to get float.")
+
+    def test_get_scalar_double(self):
 
         # double
         idl.execute("test_double = double(1.0)")
         self.assertEqual(1.0, idl.get("test_double"), "Failed to get double.")
 
+    def test_get_scalar_complex_float(self):
+
         # float complex
         idl.execute("test_fcomplex = complex(1.0, 2.0)")
         self.assertEqual(complex(1.0, 2.0), idl.get("test_fcomplex"), "Failed to get float complex.")
+
+    def test_get_scalar_complex_double(self):
 
         # double complex
         idl.execute("test_dcomplex = dcomplex(1.0, 2.0)")
         self.assertEqual(complex(1.0, 2.0), idl.get("test_dcomplex"), "Failed to get double complex.")
 
-    def test_get_array(self):
+    def test_get_array_byte(self):
 
-        pass
+        # 1D array, byte
+        idl.execute("test_array_1d_byte = byte([1,2,3,4,5])")
+        v = idl.get("test_array_1d_byte")
+        r = np.array([1, 2, 3, 4, 5], dtype=np.uint8)
+        self.assertArrayEqual(v, r, "Failed to get byte array.")
+
+    def test_get_array_int(self):
+
+        # 1D array, int
+        idl.execute("test_array_1d_int = fix([1,2,3,4,5])")
+        v = idl.get("test_array_1d_int")
+        r = np.array([1, 2, 3, 4, 5], dtype=np.int16)
+        self.assertArrayEqual(v, r, "Failed to get int array.")
+
+    def test_get_array_uint(self):
+
+        # 1D array, uint
+        idl.execute("test_array_1d_uint = uint([1,2,3,4,5])")
+        v = idl.get("test_array_1d_uint")
+        r = np.array([1, 2, 3, 4, 5], dtype=np.uint16)
+        self.assertArrayEqual(v, r, "Failed to get uint array.")
+
+    def test_get_array_long(self):
+
+        # 1D array, long
+        idl.execute("test_array_1d_long = long([1,2,3,4,5])")
+        v = idl.get("test_array_1d_long")
+        r = np.array([1, 2, 3, 4, 5], dtype=np.int32)
+        self.assertArrayEqual(v, r, "Failed to get long array.")
+
+    def test_get_array_ulong(self):
+
+        # 1D array, ulong
+        idl.execute("test_array_1d_ulong = ulong([1,2,3,4,5])")
+        v = idl.get("test_array_1d_ulong")
+        r = np.array([1, 2, 3, 4, 5], dtype=np.uint32)
+        self.assertArrayEqual(v, r, "Failed to get ulong array.")
+
+    def test_get_array_long64(self):
+
+        # 1D array, long64
+        idl.execute("test_array_1d_long64 = long64([1,2,3,4,5])")
+        v = idl.get("test_array_1d_long64")
+        r = np.array([1, 2, 3, 4, 5], dtype=np.int64)
+        self.assertArrayEqual(v, r, "Failed to get long64 array.")
+
+    def test_get_array_ulong64(self):
+
+        # 1D array, ulong64
+        idl.execute("test_array_1d_ulong64 = ulong64([1,2,3,4,5])")
+        v = idl.get("test_array_1d_ulong64")
+        r = np.array([1, 2, 3, 4, 5], dtype=np.uint64)
+        self.assertArrayEqual(v, r, "Failed to get ulong64 array.")
+
+    def test_get_array_float(self):
+
+        # 1D array, float
+        idl.execute("test_array_1d_float = float([1,2,3,4,5])")
+        v = idl.get("test_array_1d_float")
+        r = np.array([1, 2, 3, 4, 5], dtype=np.float32)
+        self.assertArrayEqual(v, r, "Failed to get float array.")
+
+    def test_get_array_double(self):
+
+        # 1D array, double
+        idl.execute("test_array_1d_double = double([1,2,3,4,5])")
+        v = idl.get("test_array_1d_double")
+        r = np.array([1, 2, 3, 4, 5], dtype=np.float64)
+        self.assertArrayEqual(v, r, "Failed to get double array.")
+
+    def test_get_array_complex_float(self):
+
+        # 1D array, complex float
+        idl.execute("test_array_1d_complex = complex([1,2,3,4,5], [6,7,8,9,10])")
+        v = idl.get("test_array_1d_complex")
+        r = np.array([1+6j, 2+7j, 3+8j, 4+9j, 5+10j], dtype=np.complex64)
+        self.assertArrayEqual(v, r, "Failed to get complex float array.")
+
+    def test_get_array_complex_double(self):
+
+        # 1D array, complex double
+        idl.execute("test_array_1d_dcomplex = dcomplex([1,2,3,4,5], [6,7,8,9,10])")
+        v = idl.get("test_array_1d_dcomplex")
+        r = np.array([1+6j, 2+7j, 3+8j, 4+9j, 5+10j], dtype=np.complex128)
+        self.assertArrayEqual(v, r, "Failed to get complex double array.")
+
+    def test_get_array_string(self):
+
+        # 1D array, string
+        idl.execute("test_array_1d_string = [\"dog\", \"\", \"cat\", \"fish\"]")
+        v = idl.get("test_array_1d_string")
+        r = np.array(["dog", "", "cat", "fish"])
+        self.assertArrayEqual(v, r, "Failed to get string array.")
+
+    def test_get_array_multidimensional(self):
+
+        # ND array
+        idl.execute("test_array_nd = indgen(8,7,6,5,4,3,2,1, /long)")
+        self.assertTrue(False, "Test needs to be written.")
 
     def test_get_structure(self):
 
